206444a8bfffaee5fd5c83cc6d229ce1810f6170
Chinmay Roy <39342322+chinmayroy@users.noreply.github.com>
2021-07-18 04:02:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Call two variable Lenght and Width
+
+length = int(input("Enter length of rectangle: "))
+width = int(input("Enter width of rectangle: "))
+
+#Law of Rectangle Area Determination
+RectangleArea = length * width
+
+#Print the value
+print(RectangleArea)
